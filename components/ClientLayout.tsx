e78e417b7234fefa5430805703f6aa49778d225b
ariq09 <149870376+ariq09@users.noreply.github.com>
2025-11-09 02:34:43 +0600

    Fixed navbar appearing in /login, now doesnt appear anymore

---
+++ components/ClientLayout.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Navbar from "@/components/Navbar";
+
+export default function ClientLayout({
+                                         children,
+                                     }: {
+    children: React.ReactNode;
+}) {
+    const pathname = usePathname();
+
+    return (
+        <>
+
+            {!pathname.startsWith("/login") && <Navbar />}
+
+            {children}
+        </>
+    );
+}
