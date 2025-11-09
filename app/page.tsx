1b9bdb379a74f1481cedeeca8973b40fd6554262
ariq09 <149870376+ariq09@users.noreply.github.com>
2025-11-09 19:46:29 +0600

    minor tweaks in app -> page.tsx (made it server side for better SEO)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-"use client";
-import Homepage from "@/components/Homepage";
 import { useUser } from "@/hooks/useUser";
-import { supabase } from "@/lib/supabaseClient";
-import React, { useEffect, useState } from "react";
-import GetStarted from "./get-started/page";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import Navbar from "@/components/Navbar";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
 
 const App = () => {
   const { loading } = useUser();
@@ -16,8 +15,13 @@ const App = () => {
   return (
 
     <main className="px-6 py-2">
-        <section className="bg-white h-120 rounded-2xl flex flex-row items-center justify-center">
-            hello
+        <section className="bg-white h-120 rounded-2xl flex flex-col items-center justify-between">
+            <div className="bg-amber-900 flex-1 flex flex-col items-center justify-center w-full">
+                <p className="font-bold text-5xl">New Drop</p>
+            </div>
+            <div className="bg-amber-700 flex-4 flex flex-col items-center justify-center w-full">
+                hello
+            </div>
         </section>
     </main>
   );
