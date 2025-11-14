c956594f4f5df992e2411506baeadc50fdbcb2f1
Kazi Badrul <47207000+KaziBadrul@users.noreply.github.com>
2025-11-14 12:06:41 +0600

    Update ProductCard.tsx

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,35 +1,36 @@
-import Image from "next/image"
-
+import Image from "next/image";
 
 interface Props {
-    name: string
-    image: string
-    slug: string
-    price: number
+  name: string;
+  image: string;
+  slug: string;
+  price: number;
 }
 
 const ProductCard = ({ name, image, slug, price }: Props) => {
-    return (
-        <div className="w-[20rem] h-[28rem] bg-[#d9d9d9] rounded-3xl flex flex-col items-center justify-center px-6 py-4 transform transition-all duration-300 ease-in-out hover:bg-[#8c8b8b]">
-            <div className="w-full h-[300px] flex items-center justify-center relative">
-                <Image src={image} alt={name} loading="lazy" fill className="object-cover rounded-3xl"/>
-            </div>
+  return (
+    <div className="w-[20rem] h-[28rem] bg-[#d9d9d9] rounded-3xl flex flex-col items-center justify-center px-6 py-4 transform transition-all duration-300 ease-in-out hover:bg-[#8c8b8b]">
+      <div className="w-full h-[300px] flex items-center justify-center relative">
+        <Image
+          src={image}
+          alt={name}
+          loading="lazy"
+          fill
+          className="object-cover rounded-3xl"
+        />
+      </div>
 
-            <div className="w-full max-w-full h-22">
-                <div className="h-fit">
-                    <p className="font-bold  mt-3 line-clamp-2 break-words">
-                        {name}
-                    </p>
-                </div>
-
-                <div className="mt-2">
-                    <p className="font-bold text-2xl">{price.toFixed(2)} BDT</p>
-                </div>
-
-            </div>
-
+      <div className="w-full max-w-full h-22">
+        <div className="h-fit">
+          <p className="font-bold  mt-3 line-clamp-2 break-words">{name}</p>
         </div>
-    )
-}
 
-export default ProductCard
+        <div className="mt-2">
+          <p className="font-bold text-2xl">{price.toFixed(2)} BDT</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
